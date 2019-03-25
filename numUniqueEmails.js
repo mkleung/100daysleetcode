729f35839b74aa8a2843b1cd4c540371a427528a
mike <mpakleung@gmail.com>
2019-03-25 15:12:35 -0400

    Rewrote algorithm for numUniqueEmail. Now 95% faster

--- numUniqueEmails.js
+++ numUniqueEmails.js
@@ -11,26 +11,53 @@ Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually rec
  * @param {string[]} emails
  * @return {number}
  */
+
 var numUniqueEmails = function(emails) {
 
     var emailHash = [];
 
     for (var i = 0; i < emails.length; i++) {
     	var email = emails[i];
-    	var local = email.split("@")[0].replace(/\./g,"");
-		var domain = "@" + email.split("@")[1];
-    	
-    	var filteredLocal = local.split("+")[0];
-    	console.log(filteredLocal);
+    	var local = email.substring(0, email.indexOf("@"));
+   		var domain = email.substring(email.indexOf("@"), email.length);
 
-    	emailHash[filteredLocal + domain] = 1;
+   		if (local.indexOf("+") > -1) {
+			var filteredLocal = local.substring(0, local.indexOf("+"));
+   		}
+   		else {
+   			var filteredLocal = local;
+   		}
+
+		var filteredEmail = filteredLocal.replace(/\./g, "") + domain;
+
+		emailHash[filteredEmail] = 1;
     }
 
-    return Object.keys(emailHash).length;
+  return Object.keys(emailHash).length;
 
 };
 
 
-numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]);
+// var numUniqueEmails = function(emails) {
+
+//     var emailHash = [];
+
+//     for (var i = 0; i < emails.length; i++) {
+//     	var email = emails[i];
+//     	var local = email.split("@")[0].replace(/\./g,"");
+// 		var domain = "@" + email.split("@")[1];
+    	
+//     	var filteredLocal = local.split("+")[0];
+//     	console.log(filteredLocal);
+
+//     	emailHash[filteredLocal + domain] = 1;
+//     }
+
+//     return Object.keys(emailHash).length;
+
+// };
+
+
+console.log(numUniqueEmails(["testemail@leetcode.com","testemail1@leetcode.com","testemail+david@lee.tcode.com"]));
 //console.log(numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]));
 //console.log(numUniqueEmails(["testemail@leetcode.com","testemail1@leetcode.com","testemail+david@lee.tcode.com"]));
