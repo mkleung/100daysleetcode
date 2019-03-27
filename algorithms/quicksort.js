1812779183377bc0bc535d67f6896fa6c985b6c1
mike <mpakleung@gmail.com>
2019-03-27 18:59:14 -0400

    Complete the flipAndInvertImage challenge

---
+++ algorithms/quicksort.js
@@ -0,0 +1,31 @@
+
+/*
+Implement Quicksort
+*/
+
+function quickSort(arr) {
+
+	if (arr.length <= 1) {
+		return arr;
+	}
+
+	var pivot = arr[arr.length-1];
+	var left = [];
+	var right = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] < pivot) {
+			left.push(arr[i]);
+		}
+		else {
+			right.push(arr[i]);
+		}
+	}
+
+	return [quickSort(left), pivot, quickSort(right)];
+
+}
+
+
+
+console.log(quickSort([4,3,1,5,9,2]));
---
+++ flipAndInvertImage.js
@@ -0,0 +1,36 @@
+/**
+https://leetcode.com/problems/flipping-an-image/
+
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(A) {
+	var output = [];
+	for (var l = 0; l < A.length; l++) {
+		    var temp = A[l];
+		    var i = 0;
+		    var j = temp.length - 1;
+		    while (i < j) {
+		    	var a = temp[i];
+		    	var b = temp[j];
+		    	temp[i] = b;
+		    	temp[j] = a;
+		    	i++;
+		    	j--;
+		    }
+		    var result = [];
+		    for (var k = 0; k < temp.length; k++) {
+		    	if (temp[k] == 0) {
+		    		result.push(1);
+		    	}
+		    	if (temp[k] == 1) {
+		    		result.push(0);
+		    	}
+		    }
+		    output.push(result);
+	}
+    return output;
+};
+
+
+console.log(flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]]));
