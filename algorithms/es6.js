1ab3ea121acc8a6920ec6436325d89da2ae619d7
mike <mpakleung@gmail.com>
2019-03-31 16:17:25 -0400

    Learn es6

---
+++ algorithms/es6.js
@@ -0,0 +1,15 @@
+
+function addressMaker(address) {
+
+	const {city, state} = address;
+
+	const newAddress = {
+		city,
+		state
+	};
+
+	console.log(`${newAddress.city} ${newAddress.state}`);
+
+}
+
+addressMaker({city: 'Austin', state: 'Texas'});
